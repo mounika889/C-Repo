aa0bb799741a8f2a93d1f092fcdab2574f7a160e
mounika889 <g.mounika7898@gmail.com>
2024-12-31 22:40:21 +0530

    Uploading code: Palindrome.c

--- Palindrome.c
+++ Palindrome.c
@@ -1,23 +1,21 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+int main() 
 {
-    int n,original,reversed=0;
-    scanf("%d",&n);
-    original=n;
-    int rem;
-    while(n!=0)
+    int n, reversedN = 0, remainder, originalN;
+    scanf("%d", &n);    
+    originalN = n;
+    while (n != 0) 
     {
-        rem=n%10;
-        reversed=reversed*10+n%1;
-        n/=10;
+        remainder = n % 10;
+        reversedN = reversedN * 10 + remainder;
+        n /= 10;
     }
-    if(original==reversed)
+    if (originalN == reversedN) 
     {
-        printf("Palindrome");
-    }
-    else
+        printf("Palindrome\n");
+    } else 
     {
-        printf("Not Palindrome");
+        printf("Not Palindrome\n");
     }
     return 0;
 }
